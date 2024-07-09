b526e5f720e78c500d589f1658bb5a6db4d2c6c3
Jack Micher <jack.micher26@gmail.com>
2024-07-09 11:37:26 -0400

    Add transition to initial border and color changing capabilities

--- etch.js
+++ etch.js
@@ -1,10 +1,13 @@
-const gridsizeelem = document.getElementById('gridsize');
-const gobutton = document.getElementById('sizebutton');
+const gridSizeElem = document.getElementById('gridsize');
+const goButton = document.getElementById('sizebutton');
 const mcontainer = document.getElementById('maincontainer');
+const colorElem = document.getElementById('pickcolor');
+let selectColor = colorElem.value;
 
-gobutton.addEventListener('click', () => {
+goButton.addEventListener('click', () => {
+    mcontainer.style.borderColor = "rgba(0,0,0,1)"
     mcontainer.innerHTML = "";
-    let sizer = gridsizeelem.value;
+    let sizer = gridSizeElem.value;
     if (isNaN(sizer) || sizer < 1 || sizer > 100) {
         console.log('bneis');
         return;
@@ -13,18 +16,25 @@ gobutton.addEventListener('click', () => {
         let rowdiv = document.createElement('div');
         rowdiv.classList.add('row');
         for (let h = 0; h < sizer; h++) {
+
             let celldiv = document.createElement('div');
             celldiv.classList.add('cell');
+
             celldiv.addEventListener('mousedown', () => {
-                celldiv.style.backgroundColor = '#EA0';
+                celldiv.style.backgroundColor = selectColor;
             })
             celldiv.addEventListener('mouseenter', (e) => {
                 if (e.buttons === 1) {
-                    celldiv.style.backgroundColor = '#EA0';                    
+                    celldiv.style.backgroundColor = selectColor;                    
                 }
             })
+
             rowdiv.append(celldiv);
         }
         mcontainer.append(rowdiv);
     }
+})
+
+colorElem.addEventListener('input', () => {
+    selectColor = colorElem.value;
 })
